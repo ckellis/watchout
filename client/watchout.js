c580c89be95f7eecca79867d054f18d84f97e602
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-10-16 14:12:00 -0700

    Trying to implement draggable player

--- client/watchout.js
+++ client/watchout.js
@@ -1,25 +1,40 @@
 // start slingin' some d3 here.
 (function(){
+  var width = 600;
+  var height = 400;
+
   var gameBoard = d3.select('.board').append('svg')
-                .attr('width', 600)
-                .attr('height', 400)
+                .attr('width', width)
+                .attr('height', height)
                 .style('background-color', 'red')
                 .style('background-position', 'center');
 
   var enemies = 15;
 
-  var axes = {
-    x: d3.scale.linear().domain([0,100]).range([0,600]),
-    y: d3.scale.linear().domain([0,100]).range([0,400])
-  };
+  var drag = d3.behavior.drag()
+             .on('dragstart', function() { circle.style('fill', 'red'); })
+             .on('drag', function() { circle.attr('cx', d3.event.x)
+                                            .attr('cy', d3.event.y); })
+             .on('dragend', function() { circle.style('fill', 'black'); });
+
+  var player = d3.select('svg').selectAll('.draggable')
+                .data([{ x: width/2, y: height/2, r: 10 }])
+                .enter()
+                .append('circle')
+                .attr('class', 'player')
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('r', function(d) { return d.r; })
+                .call(drag)
+                .style('fill', 'yellow');
 
   for(var i = 0; i < enemies; i++){
     d3.select('svg').append('circle')
                 .attr('class', 'enemy')
                 .attr('width', 20)
                 .attr('height', 20)
-                .attr('cx', Math.random() * (590 - 10) + 10)
-                .attr('cy', Math.random() * (390 - 10) + 10)
+                .attr('cx', Math.random() * ((width-10) - 10) + 10)
+                .attr('cy', Math.random() * ((height-10) - 10) + 10)
                 .attr('r', 10)
                 .style('fill', 'black');
   }
@@ -32,13 +47,15 @@
                   d3.select(this)
                   .transition()
                   .duration(500)
-                  .attr('cx', Math.random() * (590 - 10) + 10)
-                  .attr('cy', Math.random() * (390 - 10) + 10);
+                  .attr('cx', Math.random() * ((width-10) - 10) + 10)
+                  .attr('cy', Math.random() * ((height-10) - 10) + 10);
                 });
     setTimeout(moveEnemies, 1000);
   })();
 
+  var testCollision = function(){
 
+  };
 
 })();
 
