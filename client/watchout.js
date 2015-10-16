cc7cc1c0988a40b638274d759e3bf8a6c5faa5a1
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-10-16 13:38:59 -0700

    Creates an initializing function that appends enemies and an svg canvas to <body>; also implements a transition that moves each enemy to randomly generated positions on the canvas each second

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,44 @@
 // start slingin' some d3 here.
+(function(){
+  var gameBoard = d3.select('.board').append('svg')
+                .attr('width', 600)
+                .attr('height', 400)
+                .style('background-color', 'red')
+                .style('background-position', 'center');
+
+  var enemies = 15;
+
+  var axes = {
+    x: d3.scale.linear().domain([0,100]).range([0,600]),
+    y: d3.scale.linear().domain([0,100]).range([0,400])
+  };
+
+  for(var i = 0; i < enemies; i++){
+    d3.select('svg').append('circle')
+                .attr('class', 'enemy')
+                .attr('width', 20)
+                .attr('height', 20)
+                .attr('cx', Math.random() * (590 - 10) + 10)
+                .attr('cy', Math.random() * (390 - 10) + 10)
+                .attr('r', 10)
+                .style('fill', 'black');
+  }
+
+  (function moveEnemies(){
+    d3.select('svg').selectAll('.enemy')
+                .transition()
+                .duration(500)
+                .each('end', function() {
+                  d3.select(this)
+                  .transition()
+                  .duration(500)
+                  .attr('cx', Math.random() * (590 - 10) + 10)
+                  .attr('cy', Math.random() * (390 - 10) + 10);
+                });
+    setTimeout(moveEnemies, 1000);
+  })();
+
+
+
+})();
+
